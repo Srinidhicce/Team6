23b7efd77df2ec8c1f66e788d37fd2ac1352c3b7
Admin <srinidhi.ps2022cce@gmail.com>
2023-12-07 22:48:02 +0530

    DB connection

--- src/main/java/com/example/expencetrackerapp/DatabaseConnection.java
+++ src/main/java/com/example/expencetrackerapp/DatabaseConnection.java
@@ -26,7 +26,7 @@ public class DatabaseConnection {
         return connection;
     }
 
-    private static void showErrorAlert(String title, String header, String content) {
+    private static void showErrorAlert(String title,String header,String content) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle(title);
         alert.setHeaderText(header);
